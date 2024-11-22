f97f0503d9f6a717a2a5bdc02e7d9d0d81e66d53
CWY <1019120542@qq.com>
2024-11-22 14:22:52 +0800

    add 2025

--- model.py
+++ model.py
@@ -117,6 +117,22 @@ class InsuredData2024(db.Model):
     town = db.Column(db.Enum(*EnumerateData.town))
     village = db.Column(db.Enum(*EnumerateData.village))
 
+class InsuredData2025(db.Model):
+    __tablename__ = "insured_data_2025"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    resident_self_number = db.Column(db.String(30))
+    worker_self_number = db.Column(db.String(30))
+    name = db.Column(db.String(20))
+    id_number = db.Column(db.String(18), unique=True)
+    own_expense = db.Column(db.Integer)
+    pay_date = db.Column(db.DateTime)
+    insured_state = db.Column(db.Enum(*EnumerateData.insured_state))
+    is_civil = db.Column(db.Boolean)
+    remark = db.Column(db.String(50))
+    is_account_pay = db.Column(db.Boolean)
+    town = db.Column(db.Enum(*EnumerateData.town))
+    village = db.Column(db.Enum(*EnumerateData.village))
+
 class User(db.Model):
     __tablename__ = "user"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -482,6 +498,65 @@ class SettleData2024(db.Model):
     out_diagnose = db.Column(db.String(70))
     in_id = db.Column(db.String(30))
 
+class SettleData2025(db.Model):
+    __tablename__ = "settle_data_2025"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    settle_id = db.Column(db.String(30))
+    cure_id = db.Column(db.String(30))
+    self_number = db.Column(db.String(30))
+    name = db.Column(db.String(20))
+    id_number = db.Column(db.String(18))
+    person_type = db.Column(db.Enum(*EnumerateData.person_type))
+    hospital_id = db.Column(db.String(20))
+    hospital_name = db.Column(db.String(80))
+    hospital_level = db.Column(db.Enum(*EnumerateData.hospital_level))
+    hospital_place = db.Column(db.Enum(*EnumerateData.hospital_place))
+    start_date = db.Column(db.DateTime)
+    end_date = db.Column(db.DateTime)
+    settle_date = db.Column(db.DateTime)
+    evidence_type = db.Column(db.Enum(*EnumerateData.evidence_type))
+    all_expense = db.Column(DOUBLE)
+    self_expense = db.Column(DOUBLE)
+    over_expense = db.Column(DOUBLE)
+    first_expense = db.Column(DOUBLE)
+    inner_expense = db.Column(DOUBLE)
+    start_pay = db.Column(DOUBLE)
+    overall_pay = db.Column(DOUBLE)
+    large_pay = db.Column(DOUBLE)
+    big_pay = db.Column(DOUBLE)
+    rescue_pay = db.Column(DOUBLE)
+    civil_pay = db.Column(DOUBLE)
+    other_pay = db.Column(DOUBLE)
+    all_pay = db.Column(DOUBLE)
+    cash_pay = db.Column(DOUBLE)
+    account_pay = db.Column(DOUBLE)
+    together_pay = db.Column(DOUBLE)
+    illness_name = db.Column(db.String(70))
+    cure_type = db.Column(db.Enum(*EnumerateData.cure_type))
+    overall_percent = db.Column(DOUBLE)
+    is_centre = db.Column(db.Boolean)
+    operator = db.Column(db.String(20))
+    remark = db.Column(db.String(50))
+    town = db.Column(db.Enum(*EnumerateData.town))
+    village = db.Column(db.Enum(*EnumerateData.village))
+    illness_number = db.Column(db.String(30))
+    is_mid_settle = db.Column(db.Boolean)
+    is_use_account = db.Column(db.Boolean)
+    is_valid = db.Column(db.Boolean)
+    is_refund = db.Column(db.Boolean)
+    overyear_refund = db.Column(db.Enum(*EnumerateData.overyear_refund))
+    in_department = db.Column(db.String(30))
+    out_department = db.Column(db.String(30))
+    in_bed = db.Column(db.String(30))
+    illness_bed = db.Column(db.String(30))
+    out_bed = db.Column(db.String(30))
+    in_diagnose = db.Column(db.String(70))
+    doctor_id = db.Column(db.String(30))
+    doctor_name = db.Column(db.String(30))
+    out_diagnose_id = db.Column(db.String(30))
+    out_diagnose = db.Column(db.String(70))
+    in_id = db.Column(db.String(30))
+
 class Staff(db.Model):
     __tablename__ = "staff"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
@@ -553,6 +628,7 @@ model_dict = {
     "insured_data_2022": InsuredData2022,
     "insured_data_2023": InsuredData2023,
     "insured_data_2024": InsuredData2024,
+    "insured_data_2025": InsuredData2025,
     'user': User,
     'settle_data_2019': SettleData2019,
     'settle_data_2020': SettleData2020,
@@ -560,6 +636,7 @@ model_dict = {
     'settle_data_2022': SettleData2022,
     'settle_data_2023': SettleData2023,
     'settle_data_2024': SettleData2024,
+    'settle_data_2025': SettleData2025,
     'staff': Staff,
     'check_data': CheckData,
     'notify_data': NotifyData,
